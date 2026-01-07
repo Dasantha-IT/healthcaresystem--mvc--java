4906ab657af3ca55b0ed600b5c62258c0aa65772
Dasantha-IT <IT23309524@my.sliit.lk>
2026-01-07 23:56:08 +0000

    Fix some errors

--- src/healthcare/controller/AppController.java
+++ src/healthcare/controller/AppController.java
@@ -1,14 +1,20 @@
 package healthcare.controller;
 
 import healthcare.model.AppModel;
+import healthcare.service.PrescriptionService;
+import healthcare.service.ReferralManager;
 
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 public class AppController {
     private final File baseDir = new File(System.getProperty("user.dir"));
     private final AppModel model = new AppModel(baseDir);
+
+    private final PrescriptionService prescriptionService = new PrescriptionService();
+    private final ReferralManager referralManager = ReferralManager.getInstance();
 
     public AppModel getModel() { return model; }
     public File getBaseDir() { return baseDir; }
@@ -43,5 +49,13 @@ public class AppController {
             JOptionPane.showMessageDialog(parent, "Save failed:\n" + ex.getMessage(),
                     "Error", JOptionPane.ERROR_MESSAGE);
         }
+    }
+
+    public Map<String, String> createPrescription(Map<String, String> data) throws IOException {
+        return prescriptionService.createPrescription(model, data, baseDir);
+    }
+
+    public Map<String, String> createReferral(Map<String, String> data) throws IOException {
+        return referralManager.createReferral(model, data, baseDir);
     }
 }
